bb0127bfe95cf47441bfac791cf3a8900f00ae22
rockdev55 <steve264225@gmail.com>
2018-04-06 22:54:51 -0700

    Build SoupsSalads component

---
+++ src/components/SoupsSalads.js
@@ -0,0 +1,91 @@
+import React from 'react'
+
+const SoupsSalads = () => (
+  <div style={{ textAlign: 'left' }}>
+    <p style={{ textAlign: 'left', fontSize: '20px' }}>enjoy before your main course</p>
+    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+      <div style={{ width: '550px', paddingTop: '20px', paddingBottom: '20px' }}>
+        <div style={{ marginLeft: '30px' }}>
+          <div style={{ width: '50%', fontSize: '20px' }}>
+            <strong>The Confidant - gin, lemon, cocchi americano, nocino, egg white, lavender bitters, perrier</strong>
+          </div>
+          <div style={{ width: '50%' }}>Pine &amp; lavender drive this aromatic fizz</div>
+        </div>
+      </div>
+      <div style={{ width: '550px', paddingTop: '20px', paddingBottom: '20px' }}>
+        <div style={{ marginLeft: '30px' }}>
+          <div style={{ width: '50%', fontSize: '20px' }}>
+            <strong>The Confidant - gin, lemon, cocchi americano, nocino, egg white, lavender bitters, perrier</strong>
+          </div>
+          <div style={{ width: '50%' }}>Pine &amp; lavender drive this aromatic fizz</div>
+        </div>
+      </div>
+      <div style={{ width: '550px', paddingTop: '20px', paddingBottom: '20px' }}>
+        <div style={{ marginLeft: '30px' }}>
+          <div style={{ width: '50%', fontSize: '20px' }}>
+            <strong>The Confidant - gin, lemon, cocchi americano, nocino, egg white, lavender bitters, perrier</strong>
+          </div>
+          <div style={{ width: '50%' }}>Pine &amp; lavender drive this aromatic fizz</div>
+        </div>
+      </div>
+      <div style={{ width: '550px', paddingTop: '20px', paddingBottom: '20px' }}>
+        <div style={{ marginLeft: '30px' }}>
+          <div style={{ width: '50%', fontSize: '20px' }}>
+            <strong>The Confidant - gin, lemon, cocchi americano, nocino, egg white, lavender bitters, perrier</strong>
+          </div>
+          <div style={{ width: '50%' }}>Pine &amp; lavender drive this aromatic fizz</div>
+        </div>
+      </div>
+      <div style={{ width: '550px', paddingTop: '20px', paddingBottom: '20px' }}>
+        <div style={{ marginLeft: '30px' }}>
+          <div style={{ width: '50%', fontSize: '20px' }}>
+            <strong>The Confidant - gin, lemon, cocchi americano, nocino, egg white, lavender bitters, perrier</strong>
+          </div>
+          <div style={{ width: '50%' }}>Pine &amp; lavender drive this aromatic fizz</div>
+        </div>
+      </div>
+      <div style={{ width: '550px', paddingTop: '20px', paddingBottom: '20px' }}>
+        <div style={{ marginLeft: '30px' }}>
+          <div style={{ width: '50%', fontSize: '20px' }}>
+            <strong>The Confidant - gin, lemon, cocchi americano, nocino, egg white, lavender bitters, perrier</strong>
+          </div>
+          <div style={{ width: '50%' }}>Pine &amp; lavender drive this aromatic fizz</div>
+        </div>
+      </div>
+      <div style={{ width: '550px', paddingTop: '20px', paddingBottom: '20px' }}>
+        <div style={{ marginLeft: '30px' }}>
+          <div style={{ width: '50%', fontSize: '20px' }}>
+            <strong>The Confidant - gin, lemon, cocchi americano, nocino, egg white, lavender bitters, perrier</strong>
+          </div>
+          <div style={{ width: '50%' }}>Pine &amp; lavender drive this aromatic fizz</div>
+        </div>
+      </div>
+      <div style={{ width: '550px', paddingTop: '20px', paddingBottom: '20px' }}>
+        <div style={{ marginLeft: '30px' }}>
+          <div style={{ width: '50%', fontSize: '20px' }}>
+            <strong>The Confidant - gin, lemon, cocchi americano, nocino, egg white, lavender bitters, perrier</strong>
+          </div>
+          <div style={{ width: '50%' }}>Pine &amp; lavender drive this aromatic fizz</div>
+        </div>
+      </div>
+      <div style={{ width: '550px', paddingTop: '20px', paddingBottom: '20px' }}>
+        <div style={{ marginLeft: '30px' }}>
+          <div style={{ width: '50%', fontSize: '20px' }}>
+            <strong>The Confidant - gin, lemon, cocchi americano, nocino, egg white, lavender bitters, perrier</strong>
+          </div>
+          <div style={{ width: '50%' }}>Pine &amp; lavender drive this aromatic fizz</div>
+        </div>
+      </div>
+      <div style={{ width: '550px', paddingTop: '20px', paddingBottom: '20px' }}>
+        <div style={{ marginLeft: '30px' }}>
+          <div style={{ width: '50%', fontSize: '20px' }}>
+            <strong>The Confidant - gin, lemon, cocchi americano, nocino, egg white, lavender bitters, perrier</strong>
+          </div>
+          <div style={{ width: '50%' }}>Pine &amp; lavender drive this aromatic fizz</div>
+        </div>
+      </div>
+    </div>
+  </div>
+)
+
+export default SoupsSalads
