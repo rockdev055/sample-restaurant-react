d9239c25dbbafef76109feb3dba973e2748e515e
rockdev55 <steve264225@gmail.com>
2018-04-06 23:14:43 -0700

    Update appetizers component to use MenuItemCard

--- src/components/Appetizers.js
+++ src/components/Appetizers.js
@@ -1,89 +1,20 @@
 import React from 'react'
+import MenuItemCard from './MenuItemCard'
 
 const Appetizers = () => (
   <div style={{ textAlign: 'left' }}>
     <p style={{ textAlign: 'left', fontSize: '20px' }}>excellent for sharing</p>
     <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-      <div style={{ width: '550px', paddingTop: '20px', paddingBottom: '20px' }}>
-        <div style={{ marginLeft: '30px' }}>
-          <div style={{ width: '50%', fontSize: '20px' }}>
-            <strong>The Confidant - gin, lemon, cocchi americano, nocino, egg white, lavender bitters, perrier</strong>
-          </div>
-          <div style={{ width: '50%' }}>Pine &amp; lavender drive this aromatic fizz</div>
-        </div>
-      </div>
-      <div style={{ width: '550px', paddingTop: '20px', paddingBottom: '20px' }}>
-        <div style={{ marginLeft: '30px' }}>
-          <div style={{ width: '50%', fontSize: '20px' }}>
-            <strong>The Confidant - gin, lemon, cocchi americano, nocino, egg white, lavender bitters, perrier</strong>
-          </div>
-          <div style={{ width: '50%' }}>Pine &amp; lavender drive this aromatic fizz</div>
-        </div>
-      </div>
-      <div style={{ width: '550px', paddingTop: '20px', paddingBottom: '20px' }}>
-        <div style={{ marginLeft: '30px' }}>
-          <div style={{ width: '50%', fontSize: '20px' }}>
-            <strong>The Confidant - gin, lemon, cocchi americano, nocino, egg white, lavender bitters, perrier</strong>
-          </div>
-          <div style={{ width: '50%' }}>Pine &amp; lavender drive this aromatic fizz</div>
-        </div>
-      </div>
-      <div style={{ width: '550px', paddingTop: '20px', paddingBottom: '20px' }}>
-        <div style={{ marginLeft: '30px' }}>
-          <div style={{ width: '50%', fontSize: '20px' }}>
-            <strong>The Confidant - gin, lemon, cocchi americano, nocino, egg white, lavender bitters, perrier</strong>
-          </div>
-          <div style={{ width: '50%' }}>Pine &amp; lavender drive this aromatic fizz</div>
-        </div>
-      </div>
-      <div style={{ width: '550px', paddingTop: '20px', paddingBottom: '20px' }}>
-        <div style={{ marginLeft: '30px' }}>
-          <div style={{ width: '50%', fontSize: '20px' }}>
-            <strong>The Confidant - gin, lemon, cocchi americano, nocino, egg white, lavender bitters, perrier</strong>
-          </div>
-          <div style={{ width: '50%' }}>Pine &amp; lavender drive this aromatic fizz</div>
-        </div>
-      </div>
-      <div style={{ width: '550px', paddingTop: '20px', paddingBottom: '20px' }}>
-        <div style={{ marginLeft: '30px' }}>
-          <div style={{ width: '50%', fontSize: '20px' }}>
-            <strong>The Confidant - gin, lemon, cocchi americano, nocino, egg white, lavender bitters, perrier</strong>
-          </div>
-          <div style={{ width: '50%' }}>Pine &amp; lavender drive this aromatic fizz</div>
-        </div>
-      </div>
-      <div style={{ width: '550px', paddingTop: '20px', paddingBottom: '20px' }}>
-        <div style={{ marginLeft: '30px' }}>
-          <div style={{ width: '50%', fontSize: '20px' }}>
-            <strong>The Confidant - gin, lemon, cocchi americano, nocino, egg white, lavender bitters, perrier</strong>
-          </div>
-          <div style={{ width: '50%' }}>Pine &amp; lavender drive this aromatic fizz</div>
-        </div>
-      </div>
-      <div style={{ width: '550px', paddingTop: '20px', paddingBottom: '20px' }}>
-        <div style={{ marginLeft: '30px' }}>
-          <div style={{ width: '50%', fontSize: '20px' }}>
-            <strong>The Confidant - gin, lemon, cocchi americano, nocino, egg white, lavender bitters, perrier</strong>
-          </div>
-          <div style={{ width: '50%' }}>Pine &amp; lavender drive this aromatic fizz</div>
-        </div>
-      </div>
-      <div style={{ width: '550px', paddingTop: '20px', paddingBottom: '20px' }}>
-        <div style={{ marginLeft: '30px' }}>
-          <div style={{ width: '50%', fontSize: '20px' }}>
-            <strong>The Confidant - gin, lemon, cocchi americano, nocino, egg white, lavender bitters, perrier</strong>
-          </div>
-          <div style={{ width: '50%' }}>Pine &amp; lavender drive this aromatic fizz</div>
-        </div>
-      </div>
-      <div style={{ width: '550px', paddingTop: '20px', paddingBottom: '20px' }}>
-        <div style={{ marginLeft: '30px' }}>
-          <div style={{ width: '50%', fontSize: '20px' }}>
-            <strong>The Confidant - gin, lemon, cocchi americano, nocino, egg white, lavender bitters, perrier</strong>
-          </div>
-          <div style={{ width: '50%' }}>Pine &amp; lavender drive this aromatic fizz</div>
-        </div>
-      </div>
+      <MenuItemCard />
+      <MenuItemCard />
+      <MenuItemCard />
+      <MenuItemCard />
+      <MenuItemCard />
+      <MenuItemCard />
+      <MenuItemCard />
+      <MenuItemCard />
+      <MenuItemCard />
+      <MenuItemCard />
     </div>
   </div>
 )
