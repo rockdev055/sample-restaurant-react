580bdf2f0a7cca8f5e24901b91acf0d7666a91c1
rockdev55 <steve264225@gmail.com>
2018-04-07 11:18:37 -0700

    Style menu item card

--- src/components/MenuItemCard.js
+++ src/components/MenuItemCard.js
@@ -2,7 +2,7 @@ import React from 'react'
 import lorem from 'lorem-ipsum'
 
 const MenuItemCard = () => (
-  <div style={{ width: '550px', paddingTop: '20px', paddingBottom: '20px' }}>
+  <div style={{ width: '550px', paddingTop: '20px', paddingBottom: '20px', display: 'flex' }}>
     <div style={{ marginLeft: '30px' }}>
       <div style={{ width: '50%', fontSize: '20px' }}>
         <strong>{lorem({ count: 9, units: 'words' })}</strong>
