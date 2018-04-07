c61cc161db8fa00aa07dbc6cf2235b8405425bd3
rockdev55 <steve264225@gmail.com>
2018-04-06 22:54:12 -0700

    Organize components in App

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
 import React, { Component } from 'react'
 import Header from './components/Header'
+import Intro from './components/Intro'
+import HoursLocation from './components/HoursLocation'
+import Menu from './components/Menu'
+import { Link as ScrollLink, Element } from 'react-scroll'
 import './App.css'
-import FaBeer from 'react-icons/lib/fa/angle-double-down'
+import FaArrow from 'react-icons/lib/fa/angle-double-down'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Element name="top" />
         <Header />
-        <section>
-          <div className="max-width">
-            <p>Good Eats is a restaurant located at</p>
-            <p>543 Main St, San Diego</p>
-            <p style={{ fontSize: '45px', margin: 0, marginTop: '-20px' }}>
-              <a style={{ color: 'black' }} href="#">
-                <FaBeer />
-              </a>
-            </p>
-          </div>
-        </section>
-        <section style={{ paddingTop: '200px' }}>
-          <div className="max-width">
-            <h1 style={{ fontSize: '24px', fontWeight: 200 }}>HOURS & LOCATION</h1>
-          </div>
-        </section>
+        <Intro />
+        <HoursLocation />
+        <Menu />
+        <p>
+          <ScrollLink style={{ color: 'black' }} to="top" href="" spy={true} smooth={true} offset={50} duration={1000}>
+            Back to top
+          </ScrollLink>
+        </p>
       </div>
     )
   }
